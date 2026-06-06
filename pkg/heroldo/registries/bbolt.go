82d6977fed4f1cc3bd8eafbc2b653da153caee50
Daniel Chávez <danielsaul96@gmail.com>
2026-06-05 18:30:17 -0600

    Added godocs and fixed bug where previous hashed tokens were not being deleted from the database.

--- pkg/heroldo/registries/bbolt.go
+++ pkg/heroldo/registries/bbolt.go
@@ -10,14 +10,37 @@ import (
 	"go.etcd.io/bbolt"
 )
 
+// BBoltTokenRegistry is a TokenRegistry implementation backed by a bbolt
+// database.
+//
+// Tokens are stored as SHA3-256 hashes keyed by the hash. Tokens are generated
+// as a Nano ID using the matoous/go-nanoid library.
+//
+// Tokens are doubly-indexed in two different buckets: one that maps the hashed
+// token to its username, and one that maps the username too the token. This means
+// creation and deletion have some extra bookeeping and there's some space overhead,
+// but lookups should remain fast.
+//
+// Close should be called after use, to release the associated database. Note that
+// bbolt limitations of concurrent processes apply. If the database is not released,
+// other processes will hang when attempting to get a lock on it.
 type BBoltTokenRegistry struct {
 	db          *bbolt.DB
 	tokenLength int
 }
 
+// ErrTokensBucketDoesNotExist is returned when the tokens bucket is missing
+// from the database.
 var ErrTokensBucketDoesNotExist = errors.New("tokens bucket does not exist")
+
+// ErrUsersBucketDoesNotExist is returned when the users bucket is missing from
+// the database.
 var ErrUsersBucketDoesNotExist = errors.New("users bucket does not exist")
 
+// NewBBoltTokenRegistry opens or creates a bbolt database at the given path
+// and returns a BBoltTokenRegistry.
+//
+// If tokenLength is <= 0, it defaults to 64.
 func NewBBoltTokenRegistry(path string, bboltOptions *bbolt.Options, tokenLength int) (*BBoltTokenRegistry, error) {
 	if tokenLength <= 0 {
 		tokenLength = 64
@@ -50,6 +73,14 @@ func NewBBoltTokenRegistry(path string, bboltOptions *bbolt.Options, tokenLength
 	return &BBoltTokenRegistry{db: db, tokenLength: tokenLength}, nil
 }
 
+// NewToken generates a new random token for the given username, stores its
+// SHA3-256 hash, and returns the raw token.
+//
+// If the given username already had a token associated with it, it will be
+// deleted and overwritten.
+//
+// Note that the actual token is not recoverable, by design. This return value
+// is the only chance to record or present it before it's lost.
 func (bbr *BBoltTokenRegistry) NewToken(username string) (string, error) {
 	token, err := gonanoid.New(bbr.tokenLength)
 	if err != nil {
@@ -69,11 +100,22 @@ func (bbr *BBoltTokenRegistry) NewToken(username string) (string, error) {
 			return ErrUsersBucketDoesNotExist
 		}
 
+		// Delete any other previous token associated with this username
+		previousHashedToken := usersBucket.Get([]byte(username))
+		if previousHashedToken != nil {
+			err := tokensBucket.Delete(previousHashedToken)
+			if err != nil {
+				return fmt.Errorf("while removing previous token (hash: %x) associated with this user (user: %s): %w", previousHashedToken, username, err)
+			}
+		}
+
 		err := tokensBucket.Put([]byte(hash[:]), []byte(username))
 		if err != nil {
 			return fmt.Errorf("while saving data to tokens bucket: %w", err)
 		}
 
+		// No preemptive delete necessary for user, as it will simply be overwritten
+		// if existant.
 		err = usersBucket.Put([]byte(username), []byte(hash[:]))
 		if err != nil {
 			return fmt.Errorf("while saving data to users bucket: %w", err)
@@ -88,6 +130,10 @@ func (bbr *BBoltTokenRegistry) NewToken(username string) (string, error) {
 	return token, nil
 }
 
+// VerifyToken hashes the given token and looks it up in the database.
+//
+// It returns the associated username, or an empty string if the token is not
+// found.
 func (bbr *BBoltTokenRegistry) VerifyToken(token string) (string, error) {
 	hashedToken := sha3.Sum256([]byte(token))
 
@@ -111,6 +157,8 @@ func (bbr *BBoltTokenRegistry) VerifyToken(token string) (string, error) {
 	return savedUser, nil
 }
 
+// ListTokens iterates the users bucket and returns all stored
+// username-token_hash pairs.
 func (bbr *BBoltTokenRegistry) ListTokens() ([]heroldo.TokenRegistryEntry, error) {
 	var entries []heroldo.TokenRegistryEntry
 
@@ -140,6 +188,10 @@ func (bbr *BBoltTokenRegistry) ListTokens() ([]heroldo.TokenRegistryEntry, error
 	return entries, nil
 }
 
+// DeleteTokenByUsername looks up the token hash for the given username and
+// removes the entries from both the users and tokens buckets.
+//
+// If the username didn't exist, returns a nil error.
 func (bbr *BBoltTokenRegistry) DeleteTokenByUsername(username string) error {
 	usernameBytes := []byte(username)
 
@@ -178,6 +230,10 @@ func (bbr *BBoltTokenRegistry) DeleteTokenByUsername(username string) error {
 	return nil
 }
 
+// DeleteTokenByToken hashes the given token and removes the entries from both
+// the tokens and users buckets.
+//
+// If the token didn't exist, returns a nil error.
 func (bbr *BBoltTokenRegistry) DeleteTokenByToken(token string) error {
 	tokenHash := sha3.Sum256([]byte(token))
 	tokenBytes := tokenHash[:]
@@ -217,6 +273,7 @@ func (bbr *BBoltTokenRegistry) DeleteTokenByToken(token string) error {
 	return nil
 }
 
+// Close closes the underlying bbolt database.
 func (bbr *BBoltTokenRegistry) Close() error {
 	err := bbr.db.Close()
 	if err != nil {
